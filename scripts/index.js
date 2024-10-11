707f3285ac8665d877423e64fcb6c405b7bdc1e5
Hitinoviy <vovan.korchagin.1999@gmail.com>
2024-10-11 18:33:50 +0300

    start

---
+++ scripts/index.js
@@ -0,0 +1,16 @@
+let nav = document.querySelector('.menu');
+let navLinks = nav.querySelectorAll('.menu__link');
+
+window.addEventListener('hashchange', (e)=>{
+  toggleMenuActiveClass(e.newURL)
+})
+
+const toggleMenuActiveClass = (url) => {
+  navLinks.forEach((item) => {
+    item.classList.remove('active');
+    if(item.href === url){
+      item.classList.add('active');
+    }
+  });
+};
+hitinoviy.github.io
