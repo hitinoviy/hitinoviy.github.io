d4e30745631cdebf88056692fefa4fb5d12eb619
hitinoviy <vovan.korchagin.1999@gmail.com>
2024-10-20 12:14:03 +0300

    add works section

--- scripts/index.js
+++ scripts/index.js
@@ -14,4 +14,34 @@ const toggleMenuActiveClass = (url) => {
   });
 };
 
-document.querySelector('.box').style.background = 'red';
+const form = document.querySelector('#contact__form');
+const nameInput = document.querySelector('#name');
+const emailInput = document.querySelector('#mail');
+const messageInput = document.querySelector('#message');
+
+const serviceID = 'service_be2g7bt';
+const templateID = 'template_4r8j27r';
+const publicKey = 'J6dKmnsynSowDwOcl';
+
+emailjs.init(publicKey);
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const inputData = {
+    form_name: nameInput.value,
+    user_email: emailInput.value,
+    message: messageInput.value,
+  };
+  emailjs.send(serviceID, templateID, inputData).then(
+    () => {
+      nameInput.value = '';
+      emailInput.value = '';
+      messageInput.value = '';
+      console.log('success');
+    },
+    (error) => {
+      console.log(error);
+    }
+  );
+});
